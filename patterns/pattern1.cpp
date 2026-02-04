e7885dc117e56ba201ee05450b515825b519bccb
Parul2507 <parulbudhori2003@gmail.com>
2026-02-04 21:49:51 +0530

    Pattern based problem

---
+++ patterns/pattern1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int row = 4, col = 4;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
